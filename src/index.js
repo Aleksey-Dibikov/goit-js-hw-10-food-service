52aa2c9c7a5759c13a36741feebd88615d0a2c5a
Aleksey Dibikov <dibikovaleksey@gmail.com>
2021-08-07 16:58:21 +0300

    adds templates

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import './changeTheme';
 const menuRef = document.querySelector('.js-menu');
 // function buildMenu(array) {
 //     const markup = array.map(menuCardTpl).join('');
+//     // console.log(markup);
 //     menuRef.insertAdjacentHTML('beforeend', markup);
 //  }
 // buildMenu(menu);
-const markup = menuCardTpl(menu);
+
+const markup = menu.map(menuCardTpl).join('');
 menuRef.insertAdjacentHTML('beforeend', markup);
+
+// menuRef.insertAdjacentHTML('beforeend', menuCardTpl(menu));
